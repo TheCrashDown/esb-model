4be23f8742d778526424c953878c7f1a411b76ad
crashdown <thecrashdownrus@gmail.com>
2023-05-03 01:58:36 +0300

    models experiments

--- backend/endpoints/__init__.py
+++ backend/endpoints/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from models import Message
+from models import Message, Client
 from datetime import datetime
 
 api_router = APIRouter()
@@ -8,6 +8,10 @@ api_router = APIRouter()
 
 @api_router.get("/")
 def test():
-    Message.create(content=datetime.now())
+    c = Client.create(address="1.0.1.0")
+    Message.create(content=datetime.now(), sender=c)
 
-    return [m for m in Message.select().dicts()]
+    return {
+        "msg": [m for m in Message.select().dicts()],
+        "clients": [m for m in Client.select().dicts()],
+    }
--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,11 +3,24 @@ import peewee
 from .base import BaseModel
 
 
-class Message(BaseModel):
-    content = peewee.TextField()
+class Client(BaseModel):
+    class Meta:
+        db_table = "client"
 
+    id = peewee.AutoField(primary_key=True)
+    address = peewee.TextField(null=False)
+
+
+Client.create_table()
+
+
+class Message(BaseModel):
     class Meta:
         db_table = "message"
+
+    id = peewee.AutoField(primary_key=True)
+    content = peewee.TextField()
+    sender = peewee.ForeignKeyField(model=Client, to_field="id")
 
 
 Message.create_table()
