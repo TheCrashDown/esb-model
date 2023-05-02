aaa16a6644908c2bd7a025d21e1f83504f55b143
crashdown <thecrashdownrus@gmail.com>
2023-05-02 23:53:09 +0300

    db

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 
+from models import Message
+
+from datetime import datetime
+
 app = FastAPI()
 
 @app.get("/")
 def helloworld():
-    return "212321"
+    Message.create(content=datetime.now())
+
+    return [m for m in Message.select().dicts()]
